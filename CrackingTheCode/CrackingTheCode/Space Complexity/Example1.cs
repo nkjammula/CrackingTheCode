b33d37e5816a5836f707e676141ae3ce6606c0e4
Naveen Jammula <noblenaveen@gmail.com>
2018-07-09 16:28:12 -0500

    cracking the code first commit

---
+++ CrackingTheCode/CrackingTheCode/Space Complexity/Example1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingTheCode
+{
+    static class Example1
+
+    {
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Example1.SumOfIntegers(100));
+            Console.ReadLine();
+        }
+
+        //Recursive function to get sum of integers till given number
+        public static int SumOfIntegers(int n)
+    {
+        if (n <= 0)
+        {
+            return 0;
+        }
+        return (n + SumOfIntegers(n - 1));
+    }
+    }
+}
---
+++ CrackingTheCode/CrackingTheCode/Space Complexity/Example2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingTheCode.Space_Complexity
+{
+    class Example2
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Example2.PairSumSequence(1));
+            Console.ReadLine();
+        }
+
+        public static  int PairSumSequence(int n)
+        {
+            var sum = 0;
+            for (var i = 0; i < n; i++)
+            {
+                sum += PairSum(i, i + 1);
+            }
+            return sum;
+        }
+
+        public static int PairSum(int a, int b)
+        {
+            return (a + b);
+        }
+
+    }
+}
